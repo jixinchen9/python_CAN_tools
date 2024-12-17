df217a654887c69a105239fede1bcc6f1998171a
Jixin <chenjixin2@johndeere.com>
2024-12-17 15:58:35 -0600

    added regex based message parsing, and implemeted into rip logs process, plan to update more processes in future

--- process_rip_logs.py
+++ process_rip_logs.py
@@ -7,39 +7,19 @@ This script has functions to filter out a piece of info from a CAN log, that's a
 
 
 
-# import matplotlib.pyplot as plt
-# import pandas as pd
+import matplotlib.pyplot as plt
+import pandas as pd
 # from datetime import datetime as dt
 # from datetime import timedelta
 # import os
 import tools_Parse_CAN_message
 import tools_search_dbc
+import re
 
 channel_of_interest=str(20)
 baud_rate=500000   
 bits_per_message=131
 Reference_min_time_interval=(bits_per_message-3)/baud_rate
-
-time_format='%H:%M:%S.%f'
-message_indices={'tst_0':0,
-                 'tst_1':-22,
-                 'pgn_0':-16,
-                 'pgn_3':-12,
-                 'sa_0':-12,
-                 'sa_1':-10,
-                 'pgnsa_0':-16,
-                 'pgnsa_5':-10,
-                 'cmd_0':4,
-                 'cmd_4':9,
-                 'data_0':4,
-                 'data_23':27,
-                 'channel_0':-21,
-                 'channel_1':-20,
-                 'rxtx_0':-5,
-                 'rxtx_1':-3,
-                 'priority_0':-18,
-                 'priority_1':-16,
-                 }
 
 folder_w_logs='D:\\066 shutdown_all_629_31\dourado'
 log_name='h2389_shutdown_log_001_07292024.asc'
@@ -47,56 +27,32 @@ dbc_file_path=r"D:\Generated_DBC_09262024\PodB1.dbc"
 
 all_lines=open(folder_w_logs+'\\'+log_name).readlines()
 
-#uncomment this part to check if the script can slice/extract the object attributes correctly
-test_msgs=[]
-for i in range(20,40):
-    # d8_result=all_lines[i].find("d 8")
-    message1=all_lines[i]
-    print(all_lines[i])
-    parsed_msg=tools_Parse_CAN_message.parse_pdu(all_lines[i],message_indices)
-    test_msgs.append(parsed_msg)
-
-unobject_test_msgs=tools_Parse_CAN_message.show_CAN_list(test_msgs)
-# print("type of pgnsa att:", type(test_msgs[1].PGN_SA))
-
 CAN_message_all=[]
-DTC_active_set=set()
-DTC_stored_set=set()
-authentication_response=[]
 
 signal_interested_val_0_timeseries=[]
 
-#find the starting time stamp
-for line in all_lines:
-    d8_result=line.find("d 8")
-    if d8_result!=-1:
-        first_time_stamp=float(line[slice(0,d8_result-25)])
-        break
-
-signal_interested_0="FlexpwrReq " #put in name of signal exactly as it appears in the CAN spreadsheet, with a space at end to exclude names which are subsets
+signal_interested_0="EngineSpeed " #put in name of signal exactly as it appears in the CAN spreadsheet, with a space at end to exclude names which are subsets
 signal_interested_0_dict=tools_search_dbc.find_signal(signal_interested_0, dbc_file_path)
 
 for line in all_lines:
-    
-    current_message=tools_Parse_CAN_message.parse_pdu(line, message_indices)
-    
-    if current_message is None:
-        continue
-    
+    if re.search("d\s[0-9]", line):
+        
+        current_message=tools_Parse_CAN_message.parse_pdu_regex(line)
+
     #we can filter the time series based on time stamp if we want
     # if current_message.time_stamp>1 or current_message.time_stamp<10:
     #     continue  
-    
-    #print(line)
-    CAN_message_all.append(current_message)
-    # try:
-    signal_interested_val_0=tools_search_dbc.filter_signal(current_message, signal_interested_0_dict)
-    if signal_interested_val_0 != None:
-        signal_interested_val_0_timeseries.append(signal_interested_val_0)
-    # except:
-    #     continue
 
-    # if current_message.PGN == "EF06" and current_message.cmd_byte=="64 16":
-    #     authentication_response.append(current_message)
+        CAN_message_all.append(current_message)
 
-authentication_response_unobject=tools_Parse_CAN_message.show_CAN_list(authentication_response)
+        signal_interested_val_0=tools_search_dbc.filter_signal(current_message, signal_interested_0_dict)
+        if signal_interested_val_0 != None:
+            signal_interested_val_0_timeseries.append((current_message.time_stamp, signal_interested_val_0))
+
+signal_interested_0_df=pd.DataFrame(signal_interested_val_0_timeseries,columns=['timestamp','EngineSpeed'])
+
+signal_interested_0_df.plot(y='EngineSpeed',label='DMA blocks average Busload',kind='line')
+plt.xlabel('Time')
+plt.ylabel('EngineSpeed')
+plt.title('Signal_Interested')
+plt.show()
--- tools_Parse_CAN_message.py
+++ tools_Parse_CAN_message.py
@@ -36,6 +36,86 @@ class CAN_message:
     def sort_priority(self):
         return self.time_stamp
 
+def parse_pdu_regex (line):
+
+    import re
+    
+    byte_amount_regex = "d\s[0-9]"
+    source_address_regex = "[0-9,a-z][0-9,a-z]x"
+    timestamp_regex = "[0-9]+\.[0-9]+"
+    channel_regex = " [0-9]+ "
+    rxtx_regex = "[r-t]x"
+    
+
+    
+    '''
+    execute the regex querys
+    '''
+    
+    byte_amount_query = re.search(byte_amount_regex , line)
+    byte_amount_indices = byte_amount_query.span()
+    
+    source_address_query = re.search(source_address_regex , line, re.IGNORECASE)
+    source_address_indices = source_address_query.span()
+    
+    rxtx_query = re.search(rxtx_regex, line, re.IGNORECASE)
+    
+    timestamp_query = re.search(timestamp_regex , line)
+    
+    channel_query = re.search(channel_regex , line)
+    
+    
+    '''
+    find parameters without use of regex
+    '''
+    #specify string indices of the pgn using the regex result of source address
+    pgn_slice = slice(source_address_indices[0]-4,source_address_indices[0])
+    priority_slice = slice(source_address_indices[0]-6,source_address_indices[0]-4)
+    
+    #find how many data bytes there are from the regex search result
+    byte_amount_result = int(byte_amount_query.group()[-1:])
+    
+    #slice out data bytes starting from the end of byte number field and based on number 
+    #of bytes. command bytes isnt rigorous (wont always be 2 bytes), will improve
+    data_bytes_slice = slice(byte_amount_indices[1]+1,byte_amount_indices[1]+byte_amount_result*3)
+    cmd_bytes_slice = slice(byte_amount_indices[1]+1,byte_amount_indices[1]+2*3)
+    
+    '''
+    assign to CAN parameters using regex search results or other methods
+    '''
+    
+    timestamp = float(timestamp_query.group())
+    channel = channel_query.group().strip()
+    source_address = source_address_query.group().rstrip('x')
+    rxtx = rxtx_query.group()
+    pgn = line[pgn_slice]
+    pgn_and_sa = pgn + source_address
+    priority = line[priority_slice]
+    data_bytes = line[data_bytes_slice]
+    cmd_bytes = line[cmd_bytes_slice]
+    
+    '''
+    feed parsed strings into CAN_message objects
+    '''
+    current_message=CAN_message(pgn_and_sa, pgn, source_address, timestamp, cmd_bytes, data_bytes , channel ,rxtx ,timestamp, priority)
+    #current_message = pgn_and_sa + " " + str(timestamp) + " " + data_bytes + "channel: " + channel + rxtx
+    return(current_message)
+
+test_line_01 = "   0.003325 1  08EFFF5Bx    Rx   d 8 64 57 2F FF FF 00 00 FF "
+test_line_02 = "2619.825513 20  18FFF886x       Rx   d 8 9B FD 7C 04 64 80 1A 00"
+test_output =  parse_pdu_regex(test_line_02)
+
+
+def show_CAN_list(CAN_message_list):
+    CAN_list_un_object=[]
+    for message in CAN_message_list:
+        CAN_list_un_object.append((message.time_stamp, message.time_stamp_1, message.channel, message.PGN_SA,message.data_bytes,message.cmd_byte))
+    return(CAN_list_un_object)
+
+'''
+everything beyond here is old and obsolete; parse_pdu function will be deleted after last process is updated,
+everything should use regex based parsing function
+'''
 def parse_pdu(line,data_index):
     d8_result=line.find("d 8")
     if d8_result==-1:
@@ -54,10 +134,44 @@ def parse_pdu(line,data_index):
     
     current_message=CAN_message(pgnsa, pgn, sa, timestamp, commandbyte, data,chan,receiver_trans,timestamp1, priority)
     return(current_message)
-    
 
-def show_CAN_list(CAN_message_list):
-    CAN_list_un_object=[]
-    for message in CAN_message_list:
-        CAN_list_un_object.append((message.time_stamp, message.time_stamp_1, message.channel, message.PGN_SA,message.data_bytes,message.cmd_byte))
-    return(CAN_list_un_object)
+'''
+#the old parse pdu required a manual index data structure like this to work::
+    
+time_format='%H:%M:%S.%f'
+message_indices={'tst_0':0,
+                 'tst_1':-22,
+                 'pgn_0':-16,
+                 'pgn_3':-12,
+                 'sa_0':-12,
+                 'sa_1':-10,
+                 'pgnsa_0':-16,
+                 'pgnsa_5':-10,
+                 'cmd_0':4,
+                 'cmd_4':9,
+                 'data_0':4,
+                 'data_23':27,
+                 'channel_0':-21,
+                 'channel_1':-20,
+                 'rxtx_0':-5,
+                 'rxtx_1':-3,
+                 'priority_0':-18,
+                 'priority_1':-16,
+                 }
+
+#you could do something like this to check the output of manual parsing setup
+
+#uncomment this part to check if the script can slice/extract the object attributes correctly
+test_msgs=[]
+for i in range(20,40):
+    # d8_result=all_lines[i].find("d 8")
+    message1=all_lines[i]
+    print(all_lines[i])
+    parsed_msg=tools_Parse_CAN_message.parse_pdu(all_lines[i],message_indices)
+    test_msgs.append(parsed_msg)
+
+unobject_test_msgs=tools_Parse_CAN_message.show_CAN_list(test_msgs)
+# print("type of pgnsa att:", type(test_msgs[1].PGN_SA))
+
+
+'''
